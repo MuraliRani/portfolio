ec7f12f4ea995953f87b64cfacf3d02ffe537e0e
Murali R <muralideva443@gmail.com>
2024-08-30 19:29:25 +0530

    success

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -37,7 +37,7 @@ const About = () => {
         <div className="hero-resume">
           <a
             className="anchor-link"
-            href="https://drive.google.com/file/d/1G6FiU0efQB2yNZBMfbig25NJv42MN5hq/view?usp=sharing"
+            href="https://drive.google.com/file/d/14XX69H_SGa6AHmVUqw-ocXOo6toIE323/view"
             
             target="_blank"
             rel="noopener noreferrer"
